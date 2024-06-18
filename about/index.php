b54e6596a3c277fa719453b3ea244df0167092fd
Mitchell Seitz <75960015+mseitz1998@users.noreply.github.com>
2024-06-18 16:39:34 -0400

    Working on the about page

--- about/index.php
+++ about/index.php
@@ -27,19 +27,32 @@ include '../res/header.php';
         
         <p style="font-size: 19px; text-align: left;">
 
-            My name is Mitchell Seitz, and I'm kind of a cross between a self-taught developer and a computer science 
-            student. 
+            My name is Mitchell Seitz, and I'm a third-year Comp Sci student at Thompson Rivers University.
+            I've also run a small business as a handyman, worked a variety of jobs, and helped start 
+            and run a nonprofit organization dedicated to raising support for youth in and 
+            from the foster care system. My journey to this point has been fairly unique, so I might as well tell you 
+            a bit about myself so you can understand how I got here. 
+            <br><br> 
+            During High School, I ended up homeless and then in the 
+            foster care system. In order to stay in school during this time, I switched to a 
+            school that only had space available in a trades co-op program. It was here I learned the skills 
+            that kept me going throughout young adulthood, and spent most of my time on a construction site or landscaping.
+            I graduated at 17, and at 18 I registered for a business license and began working 
+            as a handyman in my spare time to get extra money to support myself. I also worked service jobs, landscaping, 
+            demolition, construction, property maintenance, and other jobs to make ends meet. 
             <br><br>
-            I'm in my Third year of my Bachelors of Computing Science at TRU, and have been studying online 
-            for my entire university education. I chose to study online so I could juggle school, work, and activism,
-            alongside other things. As a result, I've been learning on my own, and as of right now I'm focusing on 
-            finishing school and showcasing my personal projects. 
+            While doing all of that, I still found myself curious, and started spending my spare time learning anything 
+            I could find. I started by 
+            bringing home university textbooks abandoned by students in the apartment buildings I worked in for one of my 
+            jobs, and later on started buying them at value village. I started learning Math from 
+            books in the break room at a grocery store I worked at, solving brilliant.org problems 
+            on the bus, or using the wolfram problem generators to test myself. I learned some python, started messing around
+            with Arduino and Raspbery Pi, dabbled a bit in foreign languages, and started practicing doing math in my head. 
             <br><br>
-            This portfolio is the result of that, and I look forward to 
-            taking this time to get nearer to the end of my degree while exploring projects I find personally exciting and 
-            getting ready to enter the workforce. 
+            At some point, I felt like I had caught up on learning I didn't really get a chance to do in High School, and when I 
+            was 19 I decided I wanted to pursue an University Education. However, although I learned as much as I could, 
             <br><br>
-            What you see here right now are just basic school projects - But stay tuned for bigger, more interesting things!
+
 
         </p>
 
--- readme.txt
+++ readme.txt
@@ -3,9 +3,31 @@ MS_Portfolio - See it live on mseitz.dev!
 Created by Mitchell Seitz, May 2024
 
 This is the source code for my developer portfolio website, programmed 
-using PHP, HTML, CSS, and JavaScript. 
+using PHP, HTML, CSS, and JavaScript. Please see the bottom of this 
+readme for borrowed code sources. 
+_______________________________________________________________________
 
-Big thanks to the following sources that helped me make this project:
+Notes, by topic:
+
+Remember to check chrome, firefox, safari, and edge on macos / windows / 
+ios / android / ubuntu after major changes.
+
+Background Behavior: 
+   - When changing background particle behavior, please edit 
+     MS_Portfolio/res/particles/app.js 
+   - MS_Portfolio/res/particles/particles.js has the default values. 
+   - Clear browser cache and hit refresh.
+_______________________________________________________________________
+
+Issues: 
+
+- Firefox on ubuntu reders differently than firefox on other browsers. 
+  Particles.js background animation is faster, cause unknown. 
+_______________________________________________________________________
+
+Borrowed Code
+
+Big thanks to the following sources that helped me make this project
 
 Vincent Garreau, Particles.js: 
 
@@ -15,23 +37,6 @@ Vincent Garreau, Particles.js:
    and you can find the files in res/particles. I modified them as 
    needed to suit the needs of this website. 
 
-w3 Schools, where I learned how to make the Menu system: 
+w3 Schools, I based the menu system off of:  
    1: https://www.w3schools.com/howto/howto_js_sidenav.asp
    2: https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp
-
-Notes, by topic:
-_______________________________________________________________________
-
-When changing background particle behavior, please edit 
-
-MS_Portfolio/res/particles/app.js, particles.js has the default values. 
-
-Clear browser cache and hit refresh.
-_______________________________________________________________________
-
-Notes - For Debug: 
-
-- Check chrome, firefox, safari, and edge on macos / windows / ios / android / ubuntu after major changes.
-
-- Firefox on ubuntu reders differently than firefox on other browsers. Particles.js background animation faster?
-- Header home link takes up max space - put it inside of another div and shrink it around the logo? 
